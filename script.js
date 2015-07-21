5b77b38348f99ae4cd9b5b1959e8ab9607486019
corinamarie <corina.bernstein@gmail.com>
2015-07-21 13:33:42 -0500

    fixed fruitPriceUpdates function so it increments properly

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ var userBasket = {
 //something that defines fruit prices' starting point
 	//[2] will indicate if price has gone up or down in last fiftyCents function call
 var fruitPrices = [
-	["apples", 50, ""],
+	["apples", 50, ""], 
 	["oranges", 50, ""],
 	["bananas", 50, ""],
 	["grapes", 950, ""],
@@ -24,30 +24,25 @@ var fruitPrices = [
 
 var change = "";
 
-//this will call the fruitPriceUpdates function each 15 seconds
-setInterval(fruitPriceUpdates(fruitPrices), 15000);
-
 //function that changes price for each fruit
-function fruitPriceUpdates(fruit) {
+function fruitPriceUpdates() {
 	for(var i = 0; i < fruitPrices.length; i++) {
-		fruit[i][1] = fiftyCents(fruit[i][1], i);
-		console.log(fruit[i][1]);
+		if(randomNumber(1, 2) == 1 && (fruitPrices[i][1] <= 900)){
+			fruitPrices[i][1] += 50;
+			fruitPrices[i][2] = "increase";
+			console.log("increase");
+		} else if (fruitPrices[i][1] > 50) {
+			fruitPrices[i][1] -= 50;
+			fruitPrices[i][2] = "decrease";
+			console.log("decrease");
+		};
 	}
+	console.log("pears price: " + fruitPrices[1][1] + "pears indicator: " + fruitPrices[1][2]);
 }
 
-function fiftyCents(price, i){
-	console.log(index);
-	if(randomNumber(1, 2) == 1 && (price <= 900)){
-		price += 50;
-		fruit[i][2] = "increase";
-	} else if (price > 50) {
-		price -= 50;
-		fruit[i][2] = "decrease";
+//this will call the fruitPriceUpdates function each 15 seconds
+setInterval('fruitPriceUpdates(fruitPrices);', 15000);
 
-	}
-	return price;
-}
 
-console.log(fruitPrices);
-console.log(fiftyCents(750));
+
 
